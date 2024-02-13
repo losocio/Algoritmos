034716bfa1d5b682e6acd6108af81abf0d6c7c0c
Alvaro <losocio143@gmail.com>
2024-02-13 11:17:09 +0100

    No prectica lo que predica

--- T2AlgNoRecursivos/Matriz.h
+++ T2AlgNoRecursivos/Matriz.h
@@ -4,75 +4,71 @@
 using namespace std;
 
 class Matriz {
- 	private:
-  double **matriz;
-  
-  int n_filas;
-  int n_columnas;
+    private: 
+	double ** matriz;
 
-	public:
+    int n_filas;
+    int n_columnas;
 
-	//Constructor por defecto
-	Matriz();
+    public:
 
-	//Constructor por parametros
-	Matriz(int n_filas, int n_colmunas);
+    //Constructor por defecto
+    Matriz();
 
-	//Constructor copia
-	Matriz(Matriz &matriz);
+    //Constructor por parametros
+    Matriz(int n_filas, int n_colmunas);
 
-/* Realiza la asignación entre dos objetos de la clase Matriz. Crea una nueva matriz  con el resultado.
-	   Parámetro: la matriz que se va a asignar
-	   Retorno: una nueva matriz con la matriz pasada como parámetro.
-	   Precondicion: Ninguno.
-	   Complejidad Temporal: O(n_filas*n_columnas)
-	   Complejidad Espacial: O(n_filas*n_columnas)
+    //Constructor copia
+    Matriz(Matriz & matriz);
 
-
-*/
-
-  Matriz& operator= (const Matriz &m);
+    /* Realiza la asignación entre dos objetos de la clase Matriz. Crea una nueva matriz  con el resultado.
+    	   Parámetro: la matriz que se va a asignar
+    	   Retorno: una nueva matriz con la matriz pasada como parámetro.
+    	   Precondicion: Ninguno.
+    	   Complejidad Temporal: O(n_filas*n_columnas)
+    	   Complejidad Espacial: O(n_filas*n_columnas)
 
 
-/* Permite rellenar todos los elementos de la matriz del objeto actual preguntando al usuario 
-	   Parámetro: Ninguno
-	   Retorno: Ninguno
-	   Precondicion: La matriz no debe estar vacía
-	   Complejidad Temporal: O(n_filas*n_columnas)
-	   Complejidad Espacial: O(1)
+    */
+
+    Matriz & operator = (const Matriz & m);
+
+    /* Permite rellenar todos los elementos de la matriz del objeto actual preguntando al usuario 
+    	   Parámetro: Ninguno
+    	   Retorno: Ninguno
+    	   Precondicion: La matriz no debe estar vacía
+    	   Complejidad Temporal: O(n_filas*n_columnas)
+    	   Complejidad Espacial: O(1)
 
 
-*/
-  
-  void rellenarManual();
+    */
+
+    void rellenarManual();
+
+    /* Permite rellenar todos los elementos de la matriz del objeto actual de forma aleatoria 
+    	   Parámetro: semilla que se utiliza para el generador aleatorio
+    	   Retorno: Ninguno
+    	   Precondicion: La matriz no debe estar vacía
+    	   Complejidad Temporal: O(n_filas*n_columnas)
+    	   Complejidad Espacial: O(1)
 
 
-/* Permite rellenar todos los elementos de la matriz del objeto actual de forma aleatoria 
-	   Parámetro: semilla que se utiliza para el generador aleatorio
-	   Retorno: Ninguno
-	   Precondicion: La matriz no debe estar vacía
-	   Complejidad Temporal: O(n_filas*n_columnas)
-	   Complejidad Espacial: O(1)
+    */
+
+    void rellenarAleatorio(long seed);
+
+    /* Imprime por pantalla todos los elementos de la matriz del objeto actual  
+    	   Parámetro: Ninguno
+    	   Retorno: Ninguno
+    	   Precondicion: La matriz no debe estar vacía
+    	   Complejidad Temporal: O(n_filas*n_columnas)
+    	   Complejidad Espacial: O(1)
 
 
-*/
+    */
 
-  void rellenarAleatorio(long seed);
+    void mostrarMatriz();
 
-
-
-/* Imprime por pantalla todos los elementos de la matriz del objeto actual  
-	   Parámetro: Ninguno
-	   Retorno: Ninguno
-	   Precondicion: La matriz no debe estar vacía
-	   Complejidad Temporal: O(n_filas*n_columnas)
-	   Complejidad Espacial: O(1)
-
-
-*/
-
-  void mostrarMatriz();
-  
 };
 
 #endif // MATRIZ_H

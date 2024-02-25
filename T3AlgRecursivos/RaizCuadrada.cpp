aa99acd0f3c58e0e71d8c44827c6dbec5914b1c2
Alvaro <losocio143@gmail.com>
2024-02-25 12:23:29 +0100

    El bug era que estaba multiplicando en vez de sumar al calcular la media xddd

--- T3AlgRecursivos/RaizCuadrada.cpp
+++ T3AlgRecursivos/RaizCuadrada.cpp
@@ -18,7 +18,7 @@ void raizCuadradaAprox(const float objetivo, const float minimo, const float max
     assertdomjudge(objetivo>=0 && minimo>=0 && maximo>=0 && error>0);
 
     //Calculo el valor medio y luego su cuadrado
-    float valorMedio=(minimo*maximo)/2; //NOTE valor medio de un rango, no la mitad del maximo
+    float valorMedio=(minimo+maximo)/2; //NOTE valor medio de un rango, no la mitad del maximo
     float cuadrado = valorMedio*valorMedio;
     
     //Imprimo la aproximacion
@@ -27,6 +27,7 @@ void raizCuadradaAprox(const float objetivo, const float minimo, const float max
     //Si el error es menor que es requerido se sale de la recursion
     if(fabs(cuadrado-objetivo)<=error) return;
     
+    //TODO esto causa recursividad infinita y imprime 0 para siempre
     //Si el cuadrado del valor medio en mayor que el objetivo, el valor medio pasa a ser el maximo del nuevo rango
     if(cuadrado>objetivo) raizCuadradaAprox(objetivo, minimo, valorMedio, error);
     //Si es menor, el valor medio pasa a ser el minimo del nuevo rango

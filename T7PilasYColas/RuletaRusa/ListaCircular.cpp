a9f86c6dd9d0412a3811fe547e7c845b98a05218
incilius420 <incilius420@T420.T420>
2024-04-11 19:24:08 +0200

    file

---
+++ T7PilasYColas/RuletaRusa/ListaCircular.cpp
@@ -0,0 +1,39 @@
+#include "ListaCircular.h"
+
+
+ListaCircular::ListaCircular(){
+
+}
+
+ListaCircular::string getValor(int posicion){
+
+}
+
+ListaCircular::void setValor(int posicion, string nuevoValor){
+
+}
+
+
+ListaCircular::int getN(){
+
+} 
+
+
+ListaCircular::void insertar (int posicion, string nuevoValor){
+
+}
+
+
+ListaCircular::void eliminar (int posicion){
+
+}
+
+ListaCircular::void girar (int p){
+
+}
+
+
+ListaCircular::~ListaCircular(){
+
+}
+
